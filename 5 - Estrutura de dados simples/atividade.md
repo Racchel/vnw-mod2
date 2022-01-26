8b4f4f17c18241bdc88b0d4c29f88579fe32eaf1
Racchel <racchelvelasco@gmail.com>
2022-01-26 18:40:29 -0300

    atividade sobre array

---
+++ 5 - Estrutura de dados simples/atividade.md
@@ -0,0 +1,26 @@
+[] Crie um array que receba 5 itens e exiba no console.
+
+[] Utilize um método para adicionar um nome ao inicio do array.
+
+[] Utilize um método para remover o último nome do array.
+
+[] Utilize um método para adicionar dois nomes ao fim do array.
+
+[] Utilize um método para remover o primeiro nome do array.
+
+[] Utilize um método para organizar em ordem crescente o seguinte array:[] const numbers = [7,5,6,3,8,9,2,1,4]
+
+/*
+pop(): Remove an item from the end of an array.
+push(): Add items to the end of an array.
+shift(): Remove an item from the beginning of an array.
+unshift(): Add items to the beginning of an array.
+*/
+
+/*
+arr.splice(x, y) -> remove, adiciona e substitui
+
+primeiro parâmetro: é o índice do elemento adicionado, removido ou substituído
+segundo parâmetro: é o número dos elementos que vão ser adicionados, removidos ou substituídos
+terceiro parâmetro: o elemento a ser adicionado ou substituído
+*/
---
+++ 5 - Estrutura de dados simples/script.js
@@ -0,0 +1,46 @@
+let numerosArr = [7,5,6,3,8,9,2,1,4];
+
+let musicasArr = [
+   'Eu sei que vou te amar - Tom Jobim',
+   'Seu sorriso - Sabrina Lopes',
+   'Sabe, amor - Mobi Colombo',
+   'De janeiro a janeiro - Roberta Campos',
+   'Meu lugar - Onze:20'
+]
+console.log("Array de músicas:");
+console.log(musicasArr)
+console.log("Array de números:" );
+console.log(numerosArr)
+
+
+function adicionaInicio(musicasArr, musica) {
+   musicasArr.unshift(musica)
+   return console.log(musicasArr) 
+}
+
+function adicionaFim(musicasArr, musica1, musica2) {
+   musicasArr.push(musica1, musica2)
+   return console.log(musicasArr) 
+}
+
+function removeInicio(musicasArr) {
+   musicasArr.shift()
+   return console.log(musicasArr)  
+}
+
+function removeFim(musicasArr) {
+   musicasArr.pop()
+   return console.log(musicasArr) 
+}
+
+function ordemCrescente(numerosArr) {
+   numerosArr.sort()
+   return console.log(numerosArr) 
+}
+
+
+adicionaInicio(musicasArr, 'Partilhar - Rubel')
+adicionaFim(musicasArr, 'Já pensou - Fábio Brazza', 'Explodir - ANAVITÓRIA')
+removeInicio(musicasArr)
+removeFim(musicasArr)
+ordemCrescente(numerosArr)
