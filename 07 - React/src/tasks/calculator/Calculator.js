fac42e1c1e3061f66fbadd7419b2119edef1e51a
Racchel <racchelvelasco@gmail.com>
2022-02-18 13:38:34 -0300

    calculadora funcionando

--- 07 - React/src/tasks/calculator/Calculator.js
+++ 07 - React/src/tasks/calculator/Calculator.js
@@ -7,24 +7,30 @@ export default class Calculator extends Component {
     // paleta de cores usada
     color: ['#FF9500', '#505050', '#232020', '#eded70', 'white'],
     // numero que aparece na tela
-    number: 0
+    number: 0,
+    oldNumber: 0,
+    operatorInput: '',
+    result: 0,
+    historic: '',
+    isOn: false
   }
 
   inputNumber = (e) => {
-    let input = this.state.number + e.target.value
-    let length = input.length
-    console.log(`length: ${length}`)
+    const { number } = this.state
 
-    // maximo 12 digitos
+    let input = number + e.target.value
+    let length = input.length
+
+    // maximo 12 digitos  
     if (length <= 12 ) {
-      this.state.number === 0 
+      number === 0 
         // se for o primeiro digito, substitui -> não concatena
         ? this.setState({ number: e.target.value}) 
         // concatena numeros digitados para apresentar na telinha
         : this.setState({ number: input})
     } else {
       // atingindo o limite de 12 digitos, para de concatenar
-      this.setState({ number: this.state.number}) 
+      this.setState({ number: number}) 
     }
   }
 
@@ -35,19 +41,93 @@ export default class Calculator extends Component {
 
   // calcula a porcentagem
   porcentage = () => {
-    this.setState({ number: this.state.number/ 100 }) 
+    const { number } = this.state
+
+    this.setState({ number: number/ 100 }) 
   }
 
-  opertHandle = () => {
-    if (this.state.number > 0) {
-      this.setState({ number: -this.state.number }) 
-      console.log(typeof this.state.number)
+  // inverte o sinal
+  changeSing = () => {
+    const { number } = this.state
+
+    number > 0 
+      ? this.setState({ number: - number }) 
+      : this.setState({ number: Math.abs( number) })
+  }
+
+  // realiza o calculo 
+  calculate = () => {
+    const { oldNumber, number, operatorInput } = this.state
+    const oldNumberN = parseFloat(oldNumber)
+    const numberN = parseFloat(number)
+
+    let resultado = 0
+
+    if (operatorInput === '+') {
+      resultado = oldNumberN + numberN 
     }
+    if (operatorInput === '-') {
+      resultado = oldNumberN - numberN 
+    }
+    if (operatorInput === 'X') {
+      resultado = oldNumberN * numberN 
+    }
+    if (operatorInput === '/') {
+      resultado = oldNumberN / numberN 
+    }
+    
+    this.setState({ number: resultado})
+    this.setState({ result: resultado})
+
+    this.historic(resultado)
   }
 
-  function = () => {
-    console.log('a')
+  // salva no state qual operador foi selecionado
+  opertHandle = (e) => {
+    const { number } = this.state
+
+    this.setState({ operatorInput:  e.target.value })
+    this.setState({ oldNumber:  number })
+    this.setState({ number:  0 })
   }
+
+  // apaga o ultimo numero
+  cancelEntry = () => {
+    const { number } = this.state
+
+    this.setState({ number:  number.slice(0, -1) })
+  }
+
+  // faz aparecer o historico da ultima operacao
+  historic = (resultado) => {
+    const { oldNumber, number, operatorInput } = this.state
+
+    this.setState({ 
+      historic: `${oldNumber} ${operatorInput} ${number} = ${resultado}` 
+    })
+  }
+
+  // liga a calculadora
+  turnOn = () => {
+    const { isOn } = this.state
+
+    isOn? this.setState({ isOn:  false }): this.setState({ isOn:  true })
+    console.log(isOn)
+  }
+
+  // verifica se a calculadora está ligada 
+   isCalculatorOn = (e) => {
+    const { isOn } = this.state
+    const value = e.target.value
+
+    if (isOn) {
+      if(typeof e === Number) {}
+
+    } else {
+      console.log('A calculadora está desligada! Ligue antes de usar!')
+    }
+    
+   }
 
   render() {
   
@@ -55,30 +135,32 @@ export default class Calculator extends Component {
       <div>
         <S.Container>
           <S.Wrapper>
-            <S.Screen bg={this.state.color[2]}>
+            <S.OnOff isOn={ this.state.isOn } onClick={ this.turnOn } >{ this.state.isOn? 'ON' : 'OFF' } </S.OnOff>
+            <S.Screen bg={ this.state.color[2] }>
+              <S.Historic>{ this.state.historic }</S.Historic>
               <S.Number>{ this.state.number }</S.Number>
             </S.Screen>
             <S.Keyboard>
-              <S.Button bg={ this.state.color[4] } onClick={ this.function }    value={'AC'}  > AC</S.Button>  
-              <S.Button bg={ this.state.color[4] } onClick={ this.opertHandle } value={'+/-'} > +/-</S.Button> 
-              <S.Button bg={ this.state.color[4] } onClick={ this.porcentage  } value={'%'}   > %</S.Button>   
-              <S.Button bg={ this.state.color[0] } onClick={ this.function }    value={'/'}   > /</S.Button>      
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 7 }   > 7</S.Button>  
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 8 }   > 8</S.Button>  
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 9 }   > 9</S.Button>   
-              <S.Button bg={ this.state.color[0] } onClick={ this.function }    value={'X'}   > X</S.Button>  
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 4 }   > 4</S.Button>  
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 5 }   > 5</S.Button>     
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 6 }   > 6</S.Button>     
-              <S.Button bg={ this.state.color[0] } onClick={ this.function }    value={'-'}   > -</S.Button>      
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 1 }   > 1</S.Button>      
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 2 }   > 2</S.Button>        
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 3 }   > 3</S.Button>        
-              <S.Button bg={ this.state.color[0] } onClick={ this.function }    value={'+'}   > + </S.Button>          
-              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 0 }   > 0</S.Button>   
-              <S.Button bg={ this.state.color[1] } onClick={ this.function }    value={','}   > ,</S.Button> 
-              <S.Button bg={ this.state.color[3] } onClick={ this.clear }       value={'🧹'}  > 🧹</S.Button>   
-              <S.Button bg={ this.state.color[4] } onClick={ this.function }    value={'='}   > =</S.Button>    
+              <S.Button bg={ this.state.color[4] } onClick={ this.cancelEntry } value={'CE'}  > CE  </S.Button>  
+              <S.Button bg={ this.state.color[4] } onClick={ this.changeSing  } value={'+/-'} > +/- </S.Button> 
+              <S.Button bg={ this.state.color[4] } onClick={ this.porcentage  } value={'%'}   > %   </S.Button>   
+              <S.Button bg={ this.state.color[0] } onClick={ this.opertHandle } value={'/'}   > /   </S.Button>      
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 7 }   > 7   </S.Button>  
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 8 }   > 8   </S.Button>  
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 9 }   > 9   </S.Button>   
+              <S.Button bg={ this.state.color[0] } onClick={ this.opertHandle } value='X'     > X   </S.Button>  
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 4 }   > 4   </S.Button>  
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 5 }   > 5   </S.Button>     
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 6 }   > 6   </S.Button>     
+              <S.Button bg={ this.state.color[0] } onClick={ this.opertHandle } value='-'     > -   </S.Button>      
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 1 }   > 1   </S.Button>      
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 2 }   > 2   </S.Button>        
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 3 }   > 3   </S.Button>        
+              <S.Button bg={ this.state.color[0] } onClick={ this.opertHandle } value='+'     > +   </S.Button>          
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={ 0 }   > 0   </S.Button>   
+              <S.Button bg={ this.state.color[1] } onClick={ this.inputNumber } value={'.'}   > ,   </S.Button> 
+              <S.Button bg={ this.state.color[3] } onClick={ this.clear }       value={'🧹'}  > 🧹  </S.Button>   
+              <S.Button bg={ this.state.color[4] } onClick={ this.calculate   } value='='     > =   </S.Button>    
             </S.Keyboard>
           </S.Wrapper>
         </S.Container>
--- 07 - React/src/tasks/calculator/style.js
+++ 07 - React/src/tasks/calculator/style.js
@@ -6,10 +6,13 @@ export const Container = styled.div`
    display: flex;
    justify-content: center;
    align-items: center;
+   background-color: yellow;
 `
+
 
 export const Wrapper = styled.div`
    width: 30%;
+   height: 100%;
    background-color: black;
    border-radius: 1em;
    display: flex;
@@ -17,30 +20,45 @@ export const Wrapper = styled.div`
    flex-direction: column;
    align-items: center;
    padding: 4em 1em 2em 1em;
+   position: relative;
+`
+export const OnOff = styled.button`
+   width: 4em;
+   height: 3em;
+   position: absolute;
+   background-color: ${props => props.isOn? 'green' : 'red'};
+   top: 1em;
+   left: 1.8em;
 `
 
 export const Screen = styled.div`
    width: 95%;
-   height: 6em;
+   height: 9em;
    background-color: ${props => props.bg? props.bg : 'white'};
    color: ${props => props.bg? 'white' : 'blackx'};
    border-radius: 1em;
    margin-bottom: 1em;
    display: flex;
-   justify-content: flex-end;
-   align-items: center;
-   padding: 0.5em 2.1em;
+   flex-direction: column;
+   justify-content: center;
+   align-items: flex-end;
+   padding: 0 2.1em;
    border: 2px solid white;
+`
+
+export const Historic = styled.p`
+   font-size: 14px;
+   color: white;
+`
+export const Number = styled.h1`
+   font-size: 2.9em;
+   color: white;
+   margin-top: 0.5em;
 `
 
 export const Keyboard = styled.div`
    width: 95%;
    text-align: center;
-`
-
-export const Number = styled.h1`
-   font-size: 2.9em;
-   color: white;
 `
 
 export const Button = styled.button`
